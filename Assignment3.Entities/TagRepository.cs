d3551455026352f9691764a2b89fcb33cb567a41
nkar <nkar@itu.dk>
2022-09-27 13:03:47 +0200

    done with test

--- Assignment3.Entities/TagRepository.cs
+++ Assignment3.Entities/TagRepository.cs
@@ -2,9 +2,32 @@ namespace Assignment3.Entities;
 
 public class TagRepository : ITagRepository
 {
-    (Response Response, int TagId) Create(TagCreateDTO tag)
+    private readonly KanbanContext _context;
+
+    public TagRepository(KanbanContext context)
     {
-        return (Response.Created, tag.Name)
+        _context = context;
+    }
+    (Response response, int TagId) Create(TagCreateDTO tag)
+    {
+        var entity = _context.tags.FirstOrDefault(t => t.Name == tag.Name);
+        Response response;
+
+        if (entity is null)
+        {
+            entity = new Tag(tag.Name);
+
+            _context.tags.Add(entity);
+            _context.SaveChanges();
+
+            response = Response.Created;
+        }
+        else
+        {
+            response = Response.Conflict;
+        }
+
+        return (response, entity.Id);
     }
     IReadOnlyCollection<TagDTO> ReadAll()
     {
@@ -16,7 +39,8 @@ public class TagRepository : ITagRepository
     }
     Response Update(TagUpdateDTO tag)
     {
-        return null;
+        var entity = _context.tags.Find(tag.Id);
+        Response response;
     }
     Response Delete(int tagId, bool force = false)
     {
